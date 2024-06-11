3b19db86f57f04fb93b894b05ff1a376ee3aa68d
Faridz Fathin <faridz.fathin.mff2357@gmail.com>
2024-06-12 06:45:02 +0700

    artikel sama trnsaksi

--- lib/transactions.dart
+++ lib/transactions.dart
@@ -1,19 +1,117 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
 import 'payment.dart'; // Ensure you have this import if PaymentPage is in another file
 import 'transactiondetails.dart';
 
-class Transactions extends StatelessWidget {
+class Transactions extends StatefulWidget {
   final String selectedOption;
   final Function(String) onOptionChanged;
-  final List<TransactionItem> transactionList;
+  final int userId; // Assuming you have a userId to fetch transactions
 
   const Transactions({
     Key? key,
     required this.selectedOption,
     required this.onOptionChanged,
-    this.transactionList = const [], // Default value for transactionList
+    required this.userId,
   }) : super(key: key);
+
+  @override
+  _TransactionsState createState() => _TransactionsState();
+}
+
+class _TransactionsState extends State<Transactions> {
+  List<TransactionItem> transactionList = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchTransactions(widget.userId).then((lists) {
+      setState(() {
+        if (widget.selectedOption == 'History') {
+          transactionList = lists[0];
+        } else {
+          transactionList = lists[1];
+        }
+        isLoading = false;
+      });
+    });
+  }
+
+  Future<List<List<TransactionItem>>> fetchTransactions(int userId) async {
+    List<TransactionItem> historyList = [];
+    List<TransactionItem> unpaidList = [];
+
+    try {
+      final response =
+          await http.get(Uri.parse('http://127.0.0.1:8000/api/transaksis/'));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonData = json.decode(response.body);
+
+        for (var recordData in jsonData) {
+          if (recordData['user_id'] == userId) {
+            String title = 'Loading...';
+            String status = 'Loading...';
+            int appointmentId = recordData['appointment_id'];
+
+            if (recordData['konsultasi_id'] == 0) {
+              title = 'Doctor Appointment';
+            } else {
+              title = 'Online Consultation';
+            }
+            if (recordData['status_id'] == 2) {
+              status = 'Payment Completed';
+            } else {
+              status = 'Payment Incompleted';
+            }
+
+            final appointmentResponse = await http.get(Uri.parse(
+                'http://127.0.0.1:8000/api/book_appointments/$appointmentId'));
+
+            if (appointmentResponse.statusCode == 200) {
+              final appointmentData = json.decode(appointmentResponse.body);
+
+              if (appointmentData['status_id'] >= 5) {
+                final appointmentDateTime =
+                    DateTime.parse(appointmentData['tanggal']);
+                final formattedDate =
+                    '${appointmentDateTime.day}-${appointmentDateTime.month}-${appointmentDateTime.year}';
+
+                TransactionItem transaction = TransactionItem(
+                  date: formattedDate,
+                  title: title,
+                  status: status,
+                  appointmentId: appointmentId,
+                  onPressed: (int appointmentId) {},
+                );
+
+                if (recordData['status_id'] == 2) {
+                  historyList.add(transaction);
+                } else {
+                  unpaidList.add(transaction);
+                }
+              }
+            } else {
+              print(
+                  'Failed to load appointment details. Status code: ${appointmentResponse.statusCode}');
+            }
+          }
+        }
+
+        return [historyList, unpaidList];
+      } else {
+        print(
+            'Failed to load transactions. Status code: ${response.statusCode}');
+        return [];
+      }
+    } catch (e) {
+      print('Error fetching transactions: $e');
+      return [[], []];
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +129,7 @@ class Transactions extends StatelessWidget {
         ),
         SizedBox(height: 10),
         Text(
-          selectedOption == 'On going'
+          widget.selectedOption == 'On going'
               ? 'You have no ongoing transactions right now'
               : 'You have finished transactions right now',
           style: GoogleFonts.poppins(fontSize: 14),
@@ -49,15 +147,21 @@ class Transactions extends StatelessWidget {
               padding: EdgeInsets.only(left: 25.0, top: 25.0),
               child: ElevatedButton(
                 onPressed: () {
-                  onOptionChanged('History');
+                  widget.onOptionChanged('History');
+                  fetchTransactions(widget.userId).then((lists) {
+                    setState(() {
+                      transactionList = lists[0];
+                      isLoading = false;
+                    });
+                  });
                 },
                 style: ButtonStyle(
-                  backgroundColor: selectedOption == 'History'
+                  backgroundColor: widget.selectedOption == 'History'
                       ? MaterialStateProperty.all<Color>(
                           Color.fromRGBO(34, 86, 108, 1))
                       : null,
                   foregroundColor: MaterialStateProperty.all<Color>(
-                      selectedOption == 'History'
+                      widget.selectedOption == 'History'
                           ? Colors.white
                           : Colors.black),
                   padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
@@ -83,15 +187,21 @@ class Transactions extends StatelessWidget {
               padding: EdgeInsets.only(left: 25.0, top: 25.0),
               child: ElevatedButton(
                 onPressed: () {
-                  onOptionChanged('On going');
+                  widget.onOptionChanged('On going');
+                  fetchTransactions(widget.userId).then((lists) {
+                    setState(() {
+                      transactionList = lists[1];
+                      isLoading = false;
+                    });
+                  });
                 },
                 style: ButtonStyle(
-                  backgroundColor: selectedOption == 'On going'
+                  backgroundColor: widget.selectedOption == 'On going'
                       ? MaterialStateProperty.all<Color>(
                           Color.fromRGBO(34, 86, 108, 1))
                       : null,
                   foregroundColor: MaterialStateProperty.all<Color>(
-                      selectedOption == 'On going'
+                      widget.selectedOption == 'On going'
                           ? Colors.white
                           : Colors.black),
                   padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
@@ -118,29 +228,31 @@ class Transactions extends StatelessWidget {
         SizedBox(height: 10), // Adding space between buttons and list
         Expanded(
           child: Container(
-            child: transactionList.isEmpty
-                ? Padding(
-                    padding: EdgeInsets.only(top: 200),
-                    child: Center(child: noTransactionsWidget),
-                  )
-                : SingleChildScrollView(
-                    child: Column(
-                      children: List.generate(
-                        transactionList.length,
-                        (index) {
-                          // Wrap each TransactionItem with Column and add Divider after it
-                          return Column(
-                            children: [
-                              transactionList[index],
-                              Divider(color: Colors.black), // Horizontal line
-                            ],
-                          );
-                        },
+            child: isLoading
+                ? Center(child: CircularProgressIndicator())
+                : transactionList.isEmpty
+                    ? Padding(
+                        padding: EdgeInsets.only(top: 200),
+                        child: Center(child: noTransactionsWidget),
                       )
-                          .expand((widget) => [widget, SizedBox(height: 8)])
-                          .toList(), // Converts Iterable to List
-                    ),
-                  ),
+                    : SingleChildScrollView(
+                        child: Column(
+                          children: List.generate(
+                            transactionList.length,
+                            (index) {
+                              return Column(
+                                children: [
+                                  transactionList[index],
+                                  Divider(
+                                      color: Colors.black), // Horizontal line
+                                ],
+                              );
+                            },
+                          )
+                              .expand((widget) => [widget, SizedBox(height: 8)])
+                              .toList(), // Converts Iterable to List
+                        ),
+                      ),
           ),
         ),
       ],
@@ -190,21 +302,22 @@ class TransactionItem extends StatelessWidget {
               ),
               ElevatedButton(
                 onPressed: () {
-                  onPressed(
-                      appointmentId); // Pass appointmentId when button is pressed
+                  onPressed(appointmentId);
                   if (status == 'Payment Completed') {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) =>
-                              TransactionDetails(appointmentId: appointmentId)),
+                        builder: (context) =>
+                            TransactionDetails(appointmentId: appointmentId),
+                      ),
                     );
                   } else {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) =>
-                              PaymentMethods(appointmentId: appointmentId)),
+                        builder: (context) =>
+                            PaymentMethods(appointmentId: appointmentId),
+                      ),
                     );
                   }
                 },
